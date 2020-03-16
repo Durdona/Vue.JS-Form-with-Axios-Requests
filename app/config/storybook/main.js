67e5db4b9f7274f314f827a931f00369e4564266
Durdona <holisihlos@gmail.com>
2020-03-16 00:10:52 -0400

    fixed conflict

--- app/config/storybook/main.js
+++ app/config/storybook/main.js
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-// module.exports = {
-//   stories: ['../../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
-//   addons: [
-//     '@storybook/addon-actions',
-//     '@storybook/addon-knobs',
-//     '@storybook/addon-links',
-//     '@storybook/addon-notes'
-//   ]
-// }
-
 module.exports = {
-	stories: [ '../../src/**/*.components.(js|jsx|ts|tsx|mdx)' ],
-	addons: [ '@storybook/addon-actions', '@storybook/addon-knobs', '@storybook/addon-links', '@storybook/addon-notes' ]
+	stories: [ "../../src/**/*.stories.(js|jsx|ts|tsx|mdx)" ],
+	addons: [ "@storybook/addon-actions", "@storybook/addon-knobs", "@storybook/addon-links", "@storybook/addon-notes" ]
 };
-=======
-module.exports = {
-  stories: ['../../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    '@storybook/addon-links',
-    '@storybook/addon-notes'
-  ]
-}
->>>>>>> subBranch
